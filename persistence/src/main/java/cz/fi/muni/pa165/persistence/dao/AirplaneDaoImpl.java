600d7e79dec9d401f72ea2f4ae88cc4322f55b38
tomas <tomasjanicek221@gmail.com>
2020-03-27 11:01:24 +0100

    Add AirplaneDao initial Impl

--- persistence/src/main/java/cz/fi/muni/pa165/persistence/dao/AirplaneDaoImpl.java
+++ persistence/src/main/java/cz/fi/muni/pa165/persistence/dao/AirplaneDaoImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
 
 @Repository
@@ -16,35 +17,42 @@ public class AirplaneDaoImpl implements AirplaneDao{
      * @author Tomáš Janíček
      */
     @PersistenceContext
-    private EntityManager entityManager;
+    private EntityManager em;
 
     @Override
     public Airplane findFreePlaneInTimeInterval(ZonedDateTime from, ZonedDateTime to) {
+//        ArrayList<Airplane> allAirplanes = (ArrayList<Airplane>) this.findAll();
+//        allAirplanes.stream()
+//                .filter(
+//                        airplane -> airplane.getFlight()
+//                )
         return null;
     }
 
     @Override
     public Airplane findById(Long id) {
-        return null;
+        return em.find(Airplane.class, id);
     }
 
     @Override
     public Collection<Airplane> findAll() {
-        return null;
+        return em.createQuery(
+                "SELECT airplane FROM Airplane airplane", Airplane.class
+        ).getResultList();
     }
 
     @Override
     public void insertAirplane(Airplane airplane) {
-
+        em.persist(airplane);
     }
 
     @Override
     public void updateAirplane(Airplane airplane) {
-
+        em.merge(airplane);
     }
 
     @Override
     public void deleteAirplane(Airplane airplane) {
-
+        em.remove(airplane);
     }
 }
