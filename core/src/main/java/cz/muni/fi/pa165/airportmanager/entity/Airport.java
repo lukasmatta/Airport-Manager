e054bbe7d3de03d22515eb9812cd8ec460002286
Lukas Matta <lukas@matta.sk>
2020-03-27 21:51:52 +0100

    Add airport entity

---
+++ core/src/main/java/cz/muni/fi/pa165/airportmanager/entity/Airport.java
@@ -0,0 +1,30 @@
+package cz.muni.fi.pa165.airportmanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class Airport {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String city;
+
+    private String country;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
