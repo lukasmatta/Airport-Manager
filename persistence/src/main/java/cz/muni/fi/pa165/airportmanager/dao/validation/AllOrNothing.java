928e0f9f615d6bd494f62ce4440ba65e5b7c0a6f
tomas <tomasjanicek221@gmail.com>
2020-03-28 16:52:11 +0100

    Add AppContext

---
+++ persistence/src/main/java/cz/muni/fi/pa165/airportmanager/dao/validation/AllOrNothing.java
@@ -0,0 +1,28 @@
+package cz.muni.fi.pa165.airportmanager.dao.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+@Target( { TYPE })
+@Retention(RUNTIME)
+@Constraint(validatedBy = AllOrNothingValidator.class)
+@Documented
+public @interface AllOrNothing {
+
+    String message() default "All of the following fields must be all null or all filled {members}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+    
+    String [] members() default {};
+    
+    
+}
