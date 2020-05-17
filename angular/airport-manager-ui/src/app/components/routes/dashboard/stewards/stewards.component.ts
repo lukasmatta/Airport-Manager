ab442b50531fa3a6100ee97e1c4011f0115faa05
almasluffy <almas.luffy@gmail.com>
2020-05-17 23:16:28 +0600

    steward-ui

--- angular/airport-manager-ui/src/app/components/routes/dashboard/stewards/stewards.component.ts
+++ angular/airport-manager-ui/src/app/components/routes/dashboard/stewards/stewards.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { DataService } from 'src/app/services/data.service';
+
+export interface Steward {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
 
 @Component({
   selector: 'app-stewards',
@@ -7,9 +15,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StewardsComponent implements OnInit {
 
-  constructor() { }
+  dataSource = new MatTableDataSource<Steward>();
+  dataColumns = ['id', 'firstName', 'lastName'];
+  firstName: string;
+  lastName: string;
+  errorMessage: string;
+
+  constructor(private dataService: DataService) {
+    this.dataService.fetchStewards().subscribe(
+      data => {
+        this.dataSource.data = data.content;
+      }
+    );
+  }
 
   ngOnInit() {
+  }
+
+  addSteward() {
+    if (this.firstName && this.lastName) {
+      this.dataService.addSteward(this.firstName, this.lastName).subscribe(
+        (data: Steward) => {
+          this.dataSource.data = this.dataSource.data.concat([{id: data.id, firstName: data.firstName, lastName: data.lastName}]);
+        }
+      );
+    } else {
+      this.errorMessage = 'firstName and lastName are required';
+    }
+    console.log(this.dataSource.data);
   }
 
 }
--- angular/airport-manager-ui/src/app/services/data.service.ts
+++ angular/airport-manager-ui/src/app/services/data.service.ts
@@ -15,9 +15,18 @@ export class DataService {
     return this.http.get(environment.restAPI + 'airports');
   }
 
+  public fetchStewards(): Observable<any> {
+    return this.http.get(environment.restAPI + 'stewards');
+  }
+
   public addAirport(city: string, country: string) {
     const data = {'city': city, 'country': country};
     return this.http.post(environment.restAPI + 'airports/auth/create', data);
+  }
+
+  public addSteward(firstName: string, lastName: string) {
+    const data = {'firstName': firstName, 'lastName': lastName};
+    return this.http.post(environment.restAPI + 'stewards/auth/create', data);
   }
 }
 
