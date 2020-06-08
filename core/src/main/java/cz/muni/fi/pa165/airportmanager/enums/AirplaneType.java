0642241b395bf3dd6d0fd1e6918da7c2da3df39f
tomas <tomasjanicek221@gmail.com>
2020-06-08 16:27:51 +0200

    Add swagger

--- core/src/main/java/cz/muni/fi/pa165/airportmanager/enums/AirplaneType.java
+++ core/src/main/java/cz/muni/fi/pa165/airportmanager/enums/AirplaneType.java
@@ -1,5 +1,5 @@
 package cz.muni.fi.pa165.airportmanager.enums;
 
 public enum AirplaneType {
-    COMMERCIAL, PRIVATE , PROPELLER
+    COMMERCIAL, PRIVATE, PROPELLER
 }
--- rest/src/main/java/cz/muni/fi/pa165/airportmanager/rest/MyConfiguration.java
+++ rest/src/main/java/cz/muni/fi/pa165/airportmanager/rest/MyConfiguration.java
@@ -9,7 +9,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import cz.muni.fi.pa165.airportmanager.config.ServiceConfiguration;
 import cz.muni.fi.pa165.airportmanager.sampledata.SampleDataConfiguration;
-import org.h2.api.DatabaseEventListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -17,15 +16,19 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.*;
-
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -34,8 +37,9 @@ import java.util.Locale;
  *
  * @author Petr Kantek
  */
-@Configuration
 @EnableWebMvc
+@EnableSwagger2
+@Configuration
 @Import({ServiceConfiguration.class, SampleDataConfiguration.class})
 @ComponentScan(basePackages = {"cz.muni.fi.pa165.airportmanager"})
 public class MyConfiguration implements WebMvcConfigurer {
@@ -68,8 +72,26 @@ public class MyConfiguration implements WebMvcConfigurer {
         return jsonConverter;
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         converters.add(customJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
